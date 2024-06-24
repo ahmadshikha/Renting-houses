8389b7748982d21f2c1ee3197f0d478aeb0dbc90
BayanAlbaghdadi0 <ham123cam12@gmail.com>
2024-06-24 12:02:52 +0200

    end login and signup design

--- frontend/renting-houses/src/App.jsx
+++ frontend/renting-houses/src/App.jsx
@@ -1,6 +1,7 @@
 import {Routes,Route} from "react-router-dom"
 import { Signup } from "./pages/auth/signup/Signup";
 import { Login } from "./pages/auth/login/Login";
+import { Home } from "./pages/home/Home";
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
    <Routes>
     <Route path="/login" element={<Login></Login>}></Route>
     <Route path="/signup" element={<Signup></Signup>}></Route>
-    {/* <Route path="/" element={<Hom} */}
+    <Route path="/" element={<Home></Home>}></Route>
    </Routes>
     </>
     
